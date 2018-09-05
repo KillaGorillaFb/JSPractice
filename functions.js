a49c7ac63a6f857ac5f248a0cf2d64a12cb75014
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-09-05 08:54:37 -0500

    Added more practice in class

---
+++ functions.js
@@ -0,0 +1,27 @@
+ function sayhello ()
+{
+    console.log("Hello");
+}
+
+sayhello(); 
+
+
+//Return a value
+ function sayHola ()
+{
+    let hi = "Hola Silly"
+    return hi;
+}
+
+let message = sayHola();
+console.log(message); 
+
+
+ function howzIt()
+{
+    let hi = "howzit";
+    console.log("I will not print");
+     return hi;
+}
+console.log(howzit()); 
+
---
+++ parameters.js
@@ -0,0 +1,9 @@
+//Parameters
+function sayAloha(name) {
+    const message = `Aloha`;
+    return message;
+}
+let name = "Lilo";
+console.log(name);
+console.log(sayAloha());
+console.log(sayAloha("Stitch"));
