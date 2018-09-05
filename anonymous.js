4de36711a7d52d4963e8c453fb635390f10986f6
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-09-05 09:12:08 -0500

    .

--- anonymous.js
+++ anonymous.js
@@ -17,3 +17,9 @@ aloha("Nemo");
 
 const howzit = name => `Howzit, ${name}!`;
 console.log(howzit("Nani"));//Prints Howzit, Nani!
+
+console.log(Math.min(4.5, 5));
+console.log(Math.pow(6,2));
+
+//Math.ceil rounds up and Math.floor rounds down
+console.log(Math.ceil(Math.random() * 3));
