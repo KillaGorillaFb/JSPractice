de3319f3bfb3b8fed2591885da84b88dd5fbedb1
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-09-12 07:59:11 -0500

    Adding Objects.js

---
+++ objects.js
@@ -0,0 +1,13 @@
+/*Creating and Accessing an Object and its Properties
+const pen = {
+  type: "ballpoint",
+  color: "blue",
+  brand: "Bic"
+};
+
+console.log(pen.type);  // "ballpoint"
+console.log(pen.color); // "blue"
+console.log(pen.brand); // "Bic"
+
+console.log(`I write with a ${pen.color} ${pen.brand} ${pen.type} pen`);
+*/
